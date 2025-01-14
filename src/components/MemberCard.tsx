aafc93ed28c741cdbbb01295b7a4b0e6174b8afb
Yeasin Arafat <yachenarafath@gmail.com>
2025-01-14 11:07:54 +0600

    hot jar

--- src/components/MemberCard.tsx
+++ src/components/MemberCard.tsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React from "react";
 
-const MemberCard = ({img,name,role}:{
-    img:string,
-    name:string,
-    role:string
+const MemberCard = ({
+  img,
+  name,
+  role,
+}: {
+  img: string;
+  name: string;
+  role: string;
 }) => {
   return (
-    <div className=' items-center justify-start w-[300px] h-[100px] gap-4 member_card_gradient rounded-l-full rounded-r-full hidden md:flex'>
-        <img src={img} alt="" className='size-20 rounded-full object-cover'/>
-        <div className='flex flex-col gap-1 '>
-            <p className='text-sm font-semibold sour-gummy'>{name}</p>
-            <p className='text-sm font-semibold bangers-regular'>{role}</p>
-        </div>
+    <div className=" items-center justify-start w-[300px] h-[82px] gap-4 member_card_gradient rounded-l-full rounded-r-full hidden md:flex">
+      <img
+        src={img}
+        height={80}
+        width={80}
+        alt=""
+        className="size-20 rounded-full object-cover"
+      />
+      <div className="flex flex-col gap-1 ">
+        <p className="text-sm font-semibold sour-gummy">{name}</p>
+        <p className="text-sm font-semibold bangers-regular">{role}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MemberCard
+export default MemberCard;
